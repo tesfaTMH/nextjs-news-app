cf082dbc717552997569f854eaa742b410f15fcc
tesfaTMH <hagos.tesfam@gmail.com>
2025-01-06 12:32:34 +0200

    Modify the the archive route so that the vavigation will be active for different paths

---
+++ app/archive/@archive/[[...filter]]/page.jsx
@@ -0,0 +1,64 @@
+import {
+  getAvailableNewsMonths,
+  getAvailableNewsYears,
+  getNewsForYear,
+  getNewsForYearAndMonth,
+} from "@/app/lib/news";
+import NewsList from "@/components/NewsList";
+import { notFound } from "next/navigation";
+import React from "react";
+import Link from "next/link";
+
+const ArchivePageByYear = ({ params }) => {
+  const filter = params.filter;
+
+  const selectedYear = filter?.[0];
+  const selectedMonth = filter?.[1];
+
+  let links = getAvailableNewsYears();
+
+  let news;
+
+  if (selectedYear && !selectedMonth) {
+    news = getNewsForYear(selectedYear);
+    links = getAvailableNewsMonths(selectedYear);
+  }
+
+  if (selectedYear && selectedMonth) {
+    news = getNewsForYearAndMonth(selectedYear, selectedMonth);
+    links = [];
+  }
+
+  return (
+    <>
+      <header className="mb-6">
+        <nav>
+          <ul className="flex items-center justify-center gap-10 text-[#b0b0a6]">
+            {links.map((link) => {
+              const href = selectedYear
+                ? `/archive/${selectedYear}/${link}`
+                : `/archive/${link}`;
+              return (
+                <li key={link}>
+                  <Link href={href}>{link}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </header>
+      <main>
+        {news && news.length > 0 ? (
+          <NewsList listOfNews={news} />
+        ) : (
+          <p>
+            No news availble for the selected time interval. Select a year to
+            see an archived news for the selected year.
+          </p>
+        )}
+      </main>
+    </>
+  );
+};
+
+export default ArchivePageByYear;
