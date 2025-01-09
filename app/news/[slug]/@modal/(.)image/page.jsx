13c38da66d033cb29f9ae570e23c58b969adcc60
tesfaTMH <hagos.tesfam@gmail.com>
2025-01-09 23:33:30 +0200

    Update dynamic news route for combining parallel route and intercepting route for image overlay application

--- app/news/[slug]/@modal/(.)image/page.jsx
+++ app/news/[slug]/@modal/(.)image/page.jsx
@@ -1,10 +1,12 @@
+"use client";
 import { DUMMY_NEWS } from "@/data/news";
 import Image from "next/image";
-import { notFound } from "next/navigation";
+import { notFound, useRouter } from "next/navigation";
 import React from "react";
 
 const InterceptedImagePage = ({ params }) => {
-  const newsSlug = params.slug;
+  const newsSlug = React.use(params).slug;
+  const router = useRouter();
 
   const foundNewsItem = DUMMY_NEWS.find(
     (newsItem) => newsItem.slug === newsSlug
@@ -16,19 +18,21 @@ const InterceptedImagePage = ({ params }) => {
 
   return (
     <>
-      {/*<h2 className="text-white z-0">Intercepted route page</h2>*/}
-      <div className="flex items-center justify-center fixed top-0 left-0 w-full h-full bg-[rgba(0, 0, 0, 0.85)]" />
+      {/*<h2 className="text-white">Intercepted route page</h2>*/}
+      <div className="flex items-center justify-center fixed top-0 left-0 w-full h-full bg-[rgba(0, 0, 0, 0.85)] m-10 p-6" />
       <dialog
-        className="w-max-50 w-[100%] border-none rounded-lg p-2 shadow-[0 0 10px 0 #181817] bg-[#bababa]"
+        className="w-max-xl w-[100%] border-none rounded-lg p-2 shadow-[0 0 10px 0 #181817] bg-[#bababa] z-10"
         open
+        onClick={router.back}
       >
         <Image
           src={`/images/news/${foundNewsItem.image}`}
           alt={foundNewsItem.title}
-          width={200}
+          width={300}
           height={150}
           //className="fullscreen-image"
-          className="h-auto w-[25%] rounded-lg shadow-xl dark:shadow-gray-800 transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0"
+          //className="h-auto w-[25%] rounded-lg shadow-xl dark:shadow-gray-800 transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0"
+          className="w-[50%] h-auto object-cover rounded-3xl mx-auto"
         />
       </dialog>
     </>
--- app/news/[slug]/image/page.jsx
+++ app/news/[slug]/image/page.jsx
@@ -1,10 +1,11 @@
+"use client";
 import { DUMMY_NEWS } from "@/data/news";
 import Image from "next/image";
 import { notFound } from "next/navigation";
 import React from "react";
 
 const ImagePage = ({ params }) => {
-  const newsSlug = params.slug;
+  const newsSlug = React.use(params).slug;
 
   const foundNewsItem = DUMMY_NEWS.find(
     (newsItem) => newsItem.slug === newsSlug
@@ -15,13 +16,14 @@ const ImagePage = ({ params }) => {
   }
 
   return (
-    <div>
+    <div className="flex items-center justify-center">
       <Image
         src={`/images/news/${foundNewsItem.image}`}
         alt={foundNewsItem.title}
         width={300}
         height={150}
-        className="h-auto w-full rounded-lg shadow-xl dark:shadow-gray-800 transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0"
+        //className="h-auto w-[50%] rounded-lg shadow-xl dark:shadow-gray-800 transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0"
+        className="w-[50%] h-auto object-cover rounded-3xl text-center"
       />
     </div>
   );
