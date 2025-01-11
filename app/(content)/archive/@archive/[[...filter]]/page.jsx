44f369e4f605f2f4818f633f3c9b1367e24f92a9
tesfaTMH <hagos.tesfam@gmail.com>
2025-01-11 11:25:14 +0200

    Add granular data fetching with react suspense

--- app/(content)/archive/@archive/[[...filter]]/page.jsx
+++ app/(content)/archive/@archive/[[...filter]]/page.jsx
@@ -6,8 +6,67 @@ import {
 } from "@/lib/news";
 import NewsList from "@/components/NewsList";
 import { notFound } from "next/navigation";
-import React from "react";
+import React, { Suspense } from "react";
 import Link from "next/link";
+
+const FilterHeader = async ({ year, month }) => {
+  const availableYears = await getAvailableNewsYears();
+  let links = availableYears;
+
+  if (year && !month) {
+    links = getAvailableNewsMonths(year);
+  }
+
+  if (year && month) {
+    links = [];
+  }
+
+  if (
+    (year && !availableYears.includes(year)) ||
+    (month && !getAvailableNewsMonths(year).includes(month))
+  ) {
+    throw new Error("Invalid filter");
+  }
+
+  return (
+    <header className="mb-6">
+      <nav>
+        <ul className="flex items-center justify-center gap-10 text-[#b0b0a6]">
+          {links.map((link) => {
+            const href = year ? `/archive/${year}/${link}` : `/archive/${link}`;
+            return (
+              <li key={link}>
+                <Link href={href}>{link}</Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+const FilteredNews = async ({ year, month }) => {
+  let news;
+  if (year && !month) {
+    news = await getNewsForYear();
+  } else if (year && month) {
+    news = await getNewsForYearAndMonth(year, month);
+  }
+
+  let newsContent = (
+    <p>
+      No news availble for the selected time interval. Select a year to see an
+      archived news for the selected year.
+    </p>
+  );
+
+  if (news && news.length > 0) {
+    newsContent = <NewsList listOfNews={news} />;
+  }
+
+  return newsContent;
+};
 
 const ArchivePageByYear = async ({ params }) => {
   const filter = params.filter;
@@ -15,57 +74,27 @@ const ArchivePageByYear = async ({ params }) => {
   const selectedYear = filter?.[0];
   const selectedMonth = filter?.[1];
 
-  let links = await getAvailableNewsYears();
-
-  let news;
-
-  if (selectedYear && !selectedMonth) {
-    news = await getNewsForYear(selectedYear);
-    links = getAvailableNewsMonths(selectedYear);
-  }
-
-  if (selectedYear && selectedMonth) {
-    news = await getNewsForYearAndMonth(selectedYear, selectedMonth);
-    links = [];
-  }
-
-  const getAvailableYears = await getAvailableNewsYears();
-
-  if (
-    (selectedYear && !getAvailableYears.includes(selectedYear)) ||
-    (selectedMonth &&
-      !getAvailableNewsMonths(selectedYear).includes(selectedMonth))
-  ) {
-    throw new Error("Invalid filter");
-  }
-
   return (
     <>
-      <header className="mb-6">
-        <nav>
-          <ul className="flex items-center justify-center gap-10 text-[#b0b0a6]">
-            {links.map((link) => {
-              const href = selectedYear
-                ? `/archive/${selectedYear}/${link}`
-                : `/archive/${link}`;
-              return (
-                <li key={link}>
-                  <Link href={href}>{link}</Link>
-                </li>
-              );
-            })}
-          </ul>
-        </nav>
-      </header>
-      <main>
-        {news && news.length > 0 ? (
-          <NewsList listOfNews={news} />
-        ) : (
-          <p>
-            No news availble for the selected time interval. Select a year to
-            see an archived news for the selected year.
+      <Suspense
+        fallback={
+          <p className="text-sm text-center my-1 mx-auto">
+            Loading News header...
           </p>
-        )}
+        }
+      >
+        <FilterHeader year={selectedYear} month={selectedMonth} />
+      </Suspense>
+      <main>
+        <Suspense
+          fallback={
+            <p className="text-2xl text-center my-5 mx-auto">
+              Loading News ...
+            </p>
+          }
+        >
+          <FilteredNews year={selectedYear} month={selectedMonth} />
+        </Suspense>
       </main>
     </>
   );
