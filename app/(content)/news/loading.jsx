0e86029fac41798401e7611d36f390d44b0b4db1
tesfaTMH <hagos.tesfam@gmail.com>
2025-01-11 09:02:28 +0200

    Adding loading functionality to news route

---
+++ app/(content)/news/loading.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LoadingNews = () => {
+  return <p className="text-2xl text-center my-5 mx-auto">Loading...</p>;
+};
+
+export default LoadingNews;
--- app/(content)/news/page.jsx
+++ app/(content)/news/page.jsx
@@ -3,7 +3,7 @@ import { DUMMY_NEWS } from "@/data/news";
 import { getAllNews } from "@/lib/news";
 
 const NewsPage = async () => {
-  const news = getAllNews();
+  const news = await getAllNews();
   return (
     <div className="flex flex-col items-center justify-center gap-6">
       <h1>News Page</h1>
